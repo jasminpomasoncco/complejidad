a48684ec5f0d74bc2f33097700a54dd736efa4b0
Jasmin Pomasoncco <89169379+jasminpomasoncco@users.noreply.github.com>
2023-06-21 13:12:35 -0500

    feat: "Add bd excel"

--- app.py
+++ app.py
@@ -1,26 +1,15 @@
 from flask import Flask, render_template, request
+import pandas as pd
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def index():
-    return render_template('form.html', ruta_encontrada=None)
 
-@app.route('/ruta', methods=['POST'])
-def buscar_ruta():
-    aeropuerto_origen = request.form['aeropuerto_origen']
-    aeropuerto_destino = request.form['aeropuerto_destino']
-    # Aquí puedes realizar la lógica para buscar la ruta más corta entre los aeropuertos
-    # y almacenar la ruta encontrada en una variable
-    # Por ahora, solo imprimiremos los aeropuertos en la consola
-    print(f"Aeropuerto de origen: {aeropuerto_origen}")
-    print(f"Aeropuerto de destino: {aeropuerto_destino}")
+    df = pd.read_excel('Aeropuertos.xlsx', sheet_name='Aeropuertos')
     
-    # Ejemplo de ruta encontrada
-    ruta_encontrada = f"{aeropuerto_origen} -> {aeropuerto_destino} -> {aeropuerto_origen}"
+    aeropuertos = df['Nombre'].tolist()
     
-    return render_template('form.html', ruta_encontrada=ruta_encontrada)
+    return render_template('form.html', ruta_encontrada=None, aeropuertos=aeropuertos)
 
-if __name__ == '__main__':
-    app.run()
 
