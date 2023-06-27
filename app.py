72821be3cc40abda99e0ddaa88b8c9ab91aada92
Jasmin Pomasoncco <89169379+jasminpomasoncco@users.noreply.github.com>
2023-06-26 21:06:11 -0500

    Update

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from geopy import distance
 import math
 import heapq
 import numpy as np
+import matplotlib.pyplot as plt
 
 app = Flask(__name__)
 
@@ -31,7 +32,14 @@ def calcular_distancias_warshall(df):
                 if distancias[i, j] > distancias[i, k] + distancias[k, j]:
                     distancias[i, j] = distancias[i, k] + distancias[k, j]
     
+    # Ordenar la matriz de distancias como una matriz de adyacencia
+    for i in range(num_aeropuertos):
+        for j in range(num_aeropuertos):
+            if distancias[i, j] == np.inf:
+                distancias[i, j] = 0
+    
     return distancias
+    
 
 def calcular_distancia_minima_dijkstra(aeropuerto_origen, aeropuerto_destino, df, distancias):
     grafo = {}
@@ -69,12 +77,21 @@ def calcular_distancia_minima_dijkstra(aeropuerto_origen, aeropuerto_destino, df
 
     return distancia_minima
 
+def calcular_ruta(aeropuerto_origen, aeropuerto_destino):
+    # Your implementation for calculating the shortest route here
+    # This can be any algorithm such as Dijkstra's algorithm or A* search
+    
+    # Return a placeholder route for now
+    return [aeropuerto_origen, aeropuerto_destino]
+
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
     df = pd.read_excel('Aeropuertos.xlsx', sheet_name='Aeropuertos')
     aeropuertos = df['Nombre'].tolist()
 
     ruta_encontrada = None
+    graph_image = None
 
     if request.method == 'POST':
         aeropuerto_origen = request.form['aeropuerto_origen']
@@ -83,10 +100,26 @@ def index():
         distancias = calcular_distancias_warshall(df)
         distancia_minima = calcular_distancia_minima_dijkstra(aeropuerto_origen, aeropuerto_destino, df, distancias)
 
-        
+        # Calculate the shortest route using your implementation
+        shortest_route = calcular_ruta(aeropuerto_origen, aeropuerto_destino)
+
+        # Generate the graph image
+        fig, ax = plt.subplots()
+        im = ax.imshow(distancias, cmap='viridis')
+        ax.set_xticks(np.arange(len(aeropuertos)))
+        ax.set_yticks(np.arange(len(aeropuertos)))
+        plt.setp(ax.get_xticklabels(), ha="right", rotation_mode="anchor")
+        plt.colorbar(im, ax=ax, label='Distancia (km)')
+
+        # Plot the shortest route
+        route_indices = [aeropuertos.index(aeropuerto) for aeropuerto in shortest_route]
+        ax.plot(route_indices, route_indices, 'r-', linewidth=2)
+
+        plt.tight_layout()
+        graph_image = 'graph.png'
+        plt.savefig('static/' + graph_image)
+        plt.close()
+
         ruta_encontrada = f"La distancia más corta entre {aeropuerto_origen} y {aeropuerto_destino} es {distancia_minima} km."
 
-        return ruta_encontrada
-
-    return render_template('form.html', ruta_encontrada=ruta_encontrada, aeropuertos=aeropuertos)
-
+    return render_template('form.html', ruta_encontrada=ruta_encontrada, graph_image=graph_image, aeropuertos=aeropuertos)
